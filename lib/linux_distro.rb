30a36aa37af910c37ccbac6c2fe1ddaac2b051ce
Vladimir Moravec <vmoravec@suse.com>
2012-07-16 18:07:54 +0200

    Refactor repeating constants

--- lib/linux_distro.rb
+++ lib/linux_distro.rb
@@ -2,74 +2,59 @@ require 'ftools'
 
 module LinuxDistro
 
-  D_FEDORA   = :fedora
-  D_SUSE     = :suse
-  D_CENTOS   = :centos
-  D_MANDRIVA = :mandriva
-
-  DISTRO_FILES = {
-    D_FEDORA    => '/etc/fedora-release',
-    D_SUSE      => '/etc/SuSE-release',
-    D_CENTOS    => '/etc/centos-release',
-    D_MANDRIVA  => '/etc/mandriva-release',
+  DISTROS = {
+    'suse'   => { :file => '/etc/SuSE-release'   },
+    'debian' => { :file => '/etc/debian_version' },
+    'ubuntu' => { :file => '/etc/ubuntu-release' },
+    'centos' => { :file => '/etc/centos-release' },
+    'fedora' => { :file => '/etc/fedora-release' },
+    'mandriva' => { :file => '/etc/mandriva-release' }
   }
 
-  def self.chroot=(directory)
-    if ! File.exists? directory
-      raise "Directory '#{directory}' does not exist"
-    elsif ! File.directory? directory
-      raise "'#{directory}' is not a directory"
+  class << self
+
+    DISTROS.each_key do |distro_name|
+      define_method "#{distro_name}?" do
+        distro == distro
+      end
     end
 
-    # lirary needs to be reinitialized
-    reset
-    @chroot = directory
-  end
-
-  def self.distro
-    init
-
-    @distro
-  end
-
-  def self.fedora?
-    distro == D_FEDORA
-  end
-
-  def self.suse?
-    distro == D_SUSE
-  end
-
-  def self.centos?
-    distro == D_CENTOS
-  end
-
-  def self.mandriva?
-    distro == D_MANDRIVA
-  end
-
-  private
-
-  def self.detect_distro
-    # 1.) Simply detect files
-    DISTRO_FILES.each do |distro, file|
-      file = File.join(@chroot, file) if @chroot
-
-      return distro if File.exists? file
+    def chroot=(directory)
+      raise "Directory '#{directory}' does not exist" unless File.exists? directory
+      raise "'#{directory}' is not a directory"       unless File.directory? directory
+      # library needs to be reinitialized
+      reset
+      @chroot = directory
     end
 
-    raise NotImplementedError, "Your distribution was not recognized"
-  end
+    def distro
+      init
+      @distro
+    end
 
-  def self.init
-    @distro = detect_distro unless @initialized
-    @initialized = true
-  end
+    alias :name :distro
 
-  # For testing and chrooting purposes, module needs to be re-initialized
-  def self.reset
-    @initialized = false
-    @chroot = nil
-  end
+    private
 
+    def detect_distro
+      # 1.) Simply detect files
+      DISTROS.each do |distro,detect|
+        file = File.join(@chroot, detect[:file]) if @chroot
+        return distro if File.exists? detect[:file]
+      end
+
+      raise NotImplementedError, "Your distribution was not recognized"
+    end
+
+    def init
+      @distro = detect_distro unless @initialized
+      @initialized = true
+    end
+
+    # For testing and chrooting purposes, module needs to be re-initialized
+    def reset
+      @initialized = false
+      @chroot = nil
+    end
+  end
 end
